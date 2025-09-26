0e4afd26bf60e924f4e5d7c8c2aadc651815cbaa
BODYSN4TCHER00 <dcumplido04@gmail.com>
2025-09-26 08:32:29 -0600

    feat/ add email to url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LCMService from './components/LCMService';
 
 function App() {
   return (
-    <div className="App">
-      <LCMService />
-    </div>
+    <Router basename="/dcumplido04_gmail_com">
+      <Routes>
+        <Route path="/" element={<LCMService />} />
+        {/* Redirección desde la raíz absoluta */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
